1062d27e9406bc202edd7b0215961ed06d2656f1
Fayad Chowdhury <fayad.chowdhury@gmail.com>
2020-06-19 21:30:44 +0600

    Do a little refactoring

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require('express')
+require('dotenv').config()
 
+const port = process.env.PORT || 6996
 const app = express()
-const port = process.env.PORT || 3000
+
+const sequelize = require('./config/db')
+
+sequelize.authenticate()
+    .then(()=>
+    {
+        console.log("Up and running")
+    })
+    .catch((err)=>
+    {
+        console.log(`Error: ${err}`)
+    })
 
 app.listen(port, () => {
     console.log(`Server up and running on ${port}`)
