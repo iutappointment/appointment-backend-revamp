ea8356db33674a82511c38d4e757076525e602aa
Fayad Chowdhury <fayad.chowdhury@gmail.com>
2020-06-19 20:49:43 +0600

    Add necessary packages, and databasing documentation.

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express')
 
 const app = express()
+const port = process.env.PORT || 3000
 
-app.listen(3000)
+app.listen(port, () => {
+    console.log(`Server up and running on ${port}`)
+})
