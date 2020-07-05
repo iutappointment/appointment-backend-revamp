d7015ef478c74dba6c6874377e6b636b498d0c30
Fayad Chowdhury <fayad.chowdhury@gmail.com>
2020-07-05 21:37:25 +0600

    Finish patient endpoints for now

--- controllers/patient.js
+++ controllers/patient.js
@@ -37,3 +37,18 @@ exports.getSinglePatient = async (req, res) => {
         res.status(500).json({message: "Internal server error"})
     }
 }
+
+exports.deletePatientById = async (req, res) => {
+    try
+    {
+        const { patientId } = req.body
+        const delPat = await pat.destroy({where: {id: patientId}})
+        if ( delPat )
+            res.status(200).json({message: "Patient deleted successfully"})
+        else
+            res.status(404).json({message: "Patient not found"})
+    }
+    catch (e) {
+        res.status(500).json({message: "Internal server error"})
+    }
+}
--- routes/patient.js
+++ routes/patient.js
@@ -1,9 +1,10 @@
-const { completeInfoPatient, getSinglePatient } = require("../controllers/patient")
+const { completeInfoPatient, getSinglePatient, deletePatientById } = require("../controllers/patient")
 const express = require('express')
 const passport = require('../config/passport-strats')
 const router = express.Router()
 
 router.post("/updateProfile", passport.authenticate('jwtPat', {session: false}), completeInfoPatient)
 router.post("/findById", getSinglePatient)
+router.post("/deleteById", passport.authenticate('jwtPat', {session: false}), deletePatientById)
 
 module.exports = router
