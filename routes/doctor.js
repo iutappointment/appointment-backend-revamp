abd28e090630646b13bffe2266ae4119c40950a5
Fayad Chowdhury <fayad.chowdhury@gmail.com>
2020-08-09 01:21:57 +0600

    Remove the findTopInAllCategories endpoint since it is both redundant and logically fallacious

--- routes/doctor.js
+++ routes/doctor.js
@@ -1,4 +1,4 @@
-const {completeInfoDoctor, getSingleDoctorById, deleteDoctorById, findDoctor, findTopDoctors, findTopInAllCategories} = require("../controllers/doctor")
+const {completeInfoDoctor, getSingleDoctorById, deleteDoctorById, findDoctor, findTopDoctors/*, findTopInAllCategories*/} = require("../controllers/doctor")
 const express = require('express')
 const passport = require('../config/passport-strats')
 const router = express.Router()
@@ -6,7 +6,7 @@ const router = express.Router()
 router.post("/findById", getSingleDoctorById)
 router.post("/find", findDoctor)
 router.post("/findTop", findTopDoctors)
-router.post("/findTopInAllCategories", findTopInAllCategories)
+// router.post("/findTopInAllCategories", findTopInAllCategories) problematic logically and programatically
 router.post("/updateProfile", passport.authenticate('jwtDoc', {session: false}), completeInfoDoctor)
 router.post("/deleteById", passport.authenticate('jwtDoc', {session: false}), deleteDoctorById)
 
