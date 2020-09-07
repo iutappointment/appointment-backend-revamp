77a5987f3df29bc6c26acdb38920c38b06b57ab9
Fayad Chowdhury <fayad.chowdhury@gmail.com>
2020-09-08 00:39:23 +0600

    Update to use lte. Op needs to be included.

--- controllers/appointment.js
+++ controllers/appointment.js
@@ -4,7 +4,7 @@ const doc = require("../models/Doctor")
 const pat = require("../models/Patient")
 const app = require("../models/Appointment")
 const moment = require('moment')
-
+const { Op } = require('sequelize')
 
 exports.createAppointment = async (req, res) => {
     try {
@@ -151,7 +151,7 @@ exports.viewUpcomingAppointmentsPatient = async (req, res) => {
             include: [{
                 model: slot,
                 required: true,
-                where: {dateOfSlot: {$lte: moment().add(1, "week").toDate()}},
+                where: {dateOfSlot: {[Op.lte]: moment().add(1, "week").toDate()}},
                 include: [{model: doc, required: true}]
             }, {model: pat, required: true, where: {id: patientId}}]
         })
@@ -172,7 +172,7 @@ exports.viewUpcomingAppointmentsDoctor = async (req, res) => {
             include: [{
                 model: slot,
                 required: true,
-                where: {dateOfSlot: {$lte: moment().add(1, "week").toDate()}},
+                where: {dateOfSlot: {[Op.lte]: moment().add(1, "week").toDate()}},
                 include: [{model: doc, required: true, where: {id: doctorId}}]
             }, {model: pat, required: true}]
         })
