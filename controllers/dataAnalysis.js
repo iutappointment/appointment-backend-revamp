e1cc1f7c6f1a753eb38862dd7bcc9205b3087834
Fayad Chowdhury <fayad.chowdhury@gmail.com>
2020-09-15 02:12:51 +0600

    Finish up most of the data analysis endpoints

--- controllers/dataAnalysis.js
+++ controllers/dataAnalysis.js
@@ -3,26 +3,144 @@ require('dotenv').config()
 
 const pool = new Pool(
     {
-        user: process.env.DB_USER,
-        host: process.env.DB_HOST,
-        database: process.env.DB_DATABASE,
-        password: process.env.DB_PASS,
-        port: process.env.PORT,
+        connectionString: process.env.DATABASE_URL
     }
 )
 
 exports.patientAgeDist = async (req, res) => {
     try{
-        let age
-        const queryStr = `select count(*), (extract (year from (age (dob)))) as age from patients group by age having (extract (year from (age (dob)))) between ${age} and ${age+10}`
+        let values = []
+        let age = []
+        let count = []
+        const queryStr = `select count(*), (extract (year from (age (dob)))) as age from patients group by age having (extract (year from (age (dob)))) between $1 and $2`
         for ( let i = 0 ; i < 10 ; i++ )
         {
-            age = i*10;
-            const res = await pool.query(queryStr)
-            console.log(res.rows[0])
+            values[0] = i*10
+            values[1] = i*10 + 10
+            const res = await pool.query(queryStr, values)
+            if ( res.rowCount !== 0 ) {
+                age.push(res.rows[0].age)
+                count.push(parseInt(res.rows[0].count))
+            }
         }
+        res.status(200).json({x: age, y: count})
     }
     catch (e) {
         console.log(e)
+        res.status(500).json({message: "Internal server error"})
+    }
+}
+
+exports.patientAddressDist = async (req, res) => {
+    try{
+        let address = []
+        let count = []
+        const queryStr = `select count(*), address from patients group by address`
+        const result = await pool.query(queryStr)
+        if ( result.rowCount !== 0 ) {
+            for ( let i = 0 ; i < result.rowCount ; i++ )
+            {
+                if (result.rows[i].address !== null && result.rows[i].address !== null) {
+                    address.push(result.rows[i].address.toString())
+                    count.push(parseInt(result.rows[i].count))
+                }
+            }
+        }
+        res.status(200).json({x: address, y: count})
+    }
+    catch (e) {
+        console.log(e)
+        res.status(500).json({message: "Internal server error"})
+    }
+}
+
+exports.patientGenderDist = async (req, res) => {
+    try{
+        let gender = []
+        let count = []
+        const queryStr = `select count(*), gender from patients group by gender;`
+        const result = await pool.query(queryStr)
+        if ( result.rowCount !== 0 ) {
+            for ( let i = 0 ; i < result.rowCount ; i++ )
+            {
+                if (result.rows[i].gender !== null && result.rows[i].count !== null) {
+                    gender.push(result.rows[i].gender.toString())
+                    count.push(parseInt(result.rows[i].count))
+                }
+            }
+        }
+        res.status(200).json({x: gender, y: count})
+    }
+    catch (e) {
+        console.log(e)
+        res.status(500).json({message: "Internal server error"})
+    }
+}
+
+exports.doctorAddressDist = async (req, res) => {
+    try{
+        let address = []
+        let count = []
+        const queryStr = `select count(*), address from doctors group by address`
+        const result = await pool.query(queryStr)
+        if ( result.rowCount !== 0 ) {
+            for ( let i = 0 ; i < result.rowCount ; i++ )
+            {
+                if (result.rows[i].address !== null && result.rows[i].address !== null) {
+                    address.push(result.rows[i].address.toString())
+                    count.push(parseInt(result.rows[i].count))
+                }
+            }
+        }
+        res.status(200).json({x: address, y: count})
+    }
+    catch (e) {
+        console.log(e)
+        res.status(500).json({message: "Internal server error"})
+    }
+}
+
+exports.doctorSpecialtyDist = async (req, res) => {
+    try{
+        let specialty = []
+        let count = []
+        const queryStr = `select count(*), specialty from doctors group by specialty`
+        const result = await pool.query(queryStr)
+        if ( result.rowCount !== 0 ) {
+            for ( let i = 0 ; i < result.rowCount ; i++ )
+            {
+                if (result.rows[i].specialty !== null && result.rows[i].count !== null) {
+                    specialty.push(result.rows[i].specialty.toString())
+                    count.push(parseInt(result.rows[i].count))
+                }
+            }
+        }
+        res.status(200).json({x: specialty, y: count})
+    }
+    catch (e) {
+        console.log(e)
+        res.status(500).json({message: "Internal server error"})
+    }
+}
+
+exports.doctorRatingDist = async (req, res) => {
+    try{
+        let values = []
+        let rating = []
+        let count = []
+        const queryStr = `select count(*) from ratings where average between $1 and $2;`
+        for ( let i = 0 ; i < 5 ; i++ )
+        {
+            values[0] = i
+            values[1] = i+1
+            const result = await pool.query(queryStr, values)
+            rating.push(parseInt(i+1))
+            count.push(parseInt(result.rows[0].count))
+        }
+        res.status(200).json({x: rating, y: count})
+    }
+    catch (e) {
+        console.log(e)
+        res.status(500).json({message: "Internal server error"})
     }
 }
--- routes/dataAnalysis.js
+++ routes/dataAnalysis.js
@@ -1,7 +1,12 @@
-const { patientAgeDist } = require("../controllers/dataAnalysis")
+const { patientAgeDist, patientAddressDist, patientGenderDist, doctorAddressDist, doctorSpecialtyDist, doctorRatingDist } = require("../controllers/dataAnalysis")
 const express = require('express')
 const router = express.Router()
 
 router.get("/patientAgeDist", patientAgeDist)
+router.get("/patientAddressDist", patientAddressDist)
+router.get("/patientGenderDist", patientGenderDist)
+router.get("/doctorAddressDist", doctorAddressDist)
+router.get("/doctorSpecialtyDist", doctorSpecialtyDist)
+router.get("/doctorRatingDist", doctorRatingDist)
 
 module.exports = router
